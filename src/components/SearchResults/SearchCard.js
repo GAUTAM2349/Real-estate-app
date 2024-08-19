90323c7e23b03c579914806aa06e4682f372ca81
Gautam <gautamguddu577@gmail.com>
2024-08-19 18:47:28 +0530

    searchResults layout without content

---
+++ src/components/SearchResults/SearchCard.js
@@ -0,0 +1,22 @@
+const SearchCard = () => {
+  return (
+    <>
+      <div className="search-card .big-search-card component-container">
+
+        <div className="property-image-container">
+          <img className="property-image"
+            src="https://img.staticmb.com/mbimages/project/Photo_h310_w462/2022/08/31/Floor-Plan-18-Purva-Atmosphere-Bangalore-5128889_600_800_310_462.jpg"
+            alt="property-image"
+          />
+        </div>
+
+        <div className="property-info"></div>
+
+        <div className="div-buttons"></div>
+        
+      </div>
+    </>
+  );
+};
+
+export default SearchCard;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,21 @@
+import SearchCard from "./SearchCard"
+import AgentsCard from "../Agents/AgentsCard";
+const SearchResults = () => {
+
+    return (
+        <>
+           <div className="search-results-container">
+            
+           <SearchCard/>
+            <SearchCard/>
+            <SearchCard/>
+            <SearchCard/>
+            <SearchCard/>
+            <SearchCard/>
+           </div>
+        </>
+    )
+    
+}
+
+export default SearchResults;
